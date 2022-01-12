e7a5098ec80914c107515db4ebd4bf110789e04c
Mariia Luchnikova <luchniklav@yandex.ru>
2022-01-12 19:52:31 +0800

    add CollNumbers

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,15 +1,23 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static final int CONST = 1;
-    /*
-    TODO:
-     - реализовать методы класса CoolNumbers
-     - посчитать время поиска введимого номера в консоль в каждой из структуры данных
-     - проанализоровать полученные данные
-     */
-
     public static void main(String[] args) {
-
+        System.out.println(CoolNumbers.generateCoolNumbers());
+        while (true) {
+            System.out.println("Введите номер автомобиля для поиска:");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("stop")) {
+                break;
+            }
+            CoolNumbers.timeBruteSearchInList(input);
+            CoolNumbers.timeBinarySearchInList(input);
+            CoolNumbers.timeSearchInHashSet(input);
+            CoolNumbers.timeSearchInTreeSet(input);
+        }
     }
 }
